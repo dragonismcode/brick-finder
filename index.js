48d03e8ff6b7100b50b44aea6c28300e2420470d
dargy // wasabi <dragonismbot@protonmail.com>
2021-03-18 15:06:41 +0000

    Making it beautiful + Saves latest ID

--- index.js
+++ index.js
@@ -1,52 +1,56 @@
+var pingsPerMilisecond = "1000";
+
 const express = require("express");
 const app = express();
 const port = 3000;
 var fetch = require("node-fetch");
-var mongodb = require("mongodb");
-var latestid = "539355"
 const fs = require('fs')
 const path = require('path');
+var latestid = require('./id.json');
 
 function checkLatest() {
-  fetch("https://api.brick-hill.com/v1/user/profile?id=" + latestid).then(
-    (response) => {
-      response.json().then((data) => {
-        if (typeof data.username !== "undefined") {
-          console.log(
-            "User found! The user is: " + data.username + " (" + data.id + ")"
-          );
+    fetch("https://api.brick-hill.com/v1/user/profile?id=" + latestid.id).then(
+        (response) => {
+            response.json().then((data) => {
+                if (typeof data.id !== "undefined") {
+                    console.log(
+                        "✅ User found! The user is: " + data.username + " (" + data.id + ")"
+                    );
 
-          let newInfo = { 
-              username: data.username,
-              id: data.id, 
-              created_at: data.created_at,
-              img: data.img,
-              description: data.description
-          };
+                    let newInfo = {
+                        username: data.username,
+                        id: data.id,
+                        created_at: data.created_at,
+                        img: data.img,
+                        description: data.description
+                    };
 
-          fs.writeFileSync('file.json', JSON.stringify(newInfo));
-          console.log("Written into file.json")
+                    let newID = {
+                        id: data.id
+                    }
 
-          latestid = +latestid + 1;
-          console.log("Searching for user with the ID of: " + latestid);
-        } else {
+                    fs.writeFileSync('file.json', JSON.stringify(newInfo));
+                    console.log("// Updated API")
+                    fs.writeFileSync('id.json', JSON.stringify(newID));
+                    console.log("// Saved into id.json")
+
+                    latestid.id = +latestid.id + 1;
+                    console.log("🔍 Searching for user with the ID of: " + latestid.id);
+                } else {}
+            });
         }
-      });
-    }
-  );
+    );
 }
 
 checkLatest();
 
-setInterval(checkLatest, 1000);
+setInterval(checkLatest, pingsPerMilisecond);
 
 app.get('/api', (req, res) => {
-  console.log("Someone just used the API! No way Jose!")
-  
-  res.sendFile(path.join(__dirname, 'file.json'));
+    console.log("🔔 Someone just pinged the API!")
+    res.sendFile(path.join(__dirname, 'file.json'));
 });
 
 app.get("/", (req, res) => res.send("Wasabi made this API."));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
+app.listen(port, () => console.log(`App listening on port ${port}!`));
